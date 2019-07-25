eb67a4cf3eb3b706beb19eff31e382aee9759c41
Giulia Maia <giuliamaia2@gmail.com>
2019-07-25 12:54:26 -0300

    Testes Automatizados Legado: Giulia

---
+++ Testes/Teste_Legado_Giulia/PrimeSyloTeste/Features/Login.feature
@@ -0,0 +1,30 @@
+﻿@logins
+Feature: Login
+  Eu como usuário, desejo realizar meu login
+  Para ter acesso as funcionalidades da aplicação
+  
+  Background:
+     Given que estou no site institucional
+    	And acessei o site >> Login 
+  
+  @user_login
+  Scenario: Login do usuário com sucesso 
+    When inseri meu login
+    And inseri minha senha
+    And clico em Login
+    Then a mensagem de sucesso aparece na tela
+   
+     @user_email_incorreto
+  Scenario: Login do usuário com email incorreto 
+    When inseri meu login incorreto
+    And inseri minha senha
+    And clico em Login
+    Then o site alerta e-mail inválido
+    
+   
+     @user_senha_incorreta
+  Scenario: Login do usuário com senha incorreta 
+    When inseri meu login
+    And inseri minha senha incorreta
+    And clico em Login
+    Then o site alerta senha inválida
---
+++ Testes/Teste_Legado_Giulia/PrimeSyloTeste/Steps/LoginSteps.cs
@@ -0,0 +1,90 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using PrimeSyloTeste.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace PrimeSyloTeste.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        LoginPage loginpage = new LoginPage();
+        String perfil = "http://localhost:3000/dashboard.html";
+
+        [BeforeScenario]
+        public void Before()
+        {
+            loginpage.abrirSite();
+        }
+
+        [AfterScenario]
+        public void After()
+        {
+            loginpage.fecharSite();
+        }
+
+
+        [Given(@"que estou no site institucional")]
+        public void GivenQueEstouNoSiteInstitucional()
+        {
+            //entrada automatica em abrirsite()
+        }
+        
+        [Given(@"acessei o site >> Login")]
+        public void GivenAcesseiOSiteLogin()
+        {
+            loginpage.clicarMenuLogin();
+        }
+        
+        [When(@"inseri meu login")]
+        public void WhenInseriMeu()
+        {
+            loginpage.inserirLogin();
+        }
+        
+        [When(@"inseri minha senha")]
+        public void WhenInseriMinha()
+        {
+            loginpage.inserirSenha();
+        }
+        
+        [When(@"clico em Login")]
+        public void WhenClicoEmLogin()
+        {
+            loginpage.clicarlogin();        
+        }
+        
+        [When(@"inseri meu login incorreto")]
+        public void WhenInseriMeuLoginIncorreto()
+        {
+            loginpage.inserirLoginErrado();
+        }
+        
+        [When(@"inseri minha senha incorreta")]
+        public void WhenInseriMinhaSenhaIncorreta()
+        {
+            loginpage.inserirSenhaErrada();
+        }
+        
+        [Then(@"a mensagem de sucesso aparece na tela")]
+        public void ThenAMensagemDeApareceNaTela()
+        {
+            //problemas para confirmar!!
+            loginpage.verificarPerfil();
+        }
+        
+        [Then(@"o site alerta e-mail inválido")]
+        public void ThenOSiteAlertaE_MailInvalido()
+        {
+            loginpage.verificarAviso();
+        }
+        
+        [Then(@"o site alerta senha inválida")]
+        public void ThenOSiteAlertaSenhaInvalida()
+        {
+            loginpage.verificarAviso();
+        }
+    }
+}
