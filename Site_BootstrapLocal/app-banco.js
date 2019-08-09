d3746b8303496e8a06ed5dc0a586bfe6b57a4be9
gustavokds <gukiyoshi45@gmail.com>
2019-08-09 10:49:26 -0300

    subindo site local

---
+++ Site_BootstrapLocal/app-banco.js
@@ -0,0 +1,61 @@
+var desenvolvimento = false;
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'tcc'
+});
+
+var configuracoes = {
+    producao: {
+        server: "tcc1semestre.database.windows.net",
+        user: "bandtec",
+        password: "xlr5821#YWO",
+        database: "ProjetoBandTec",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    },
+    desenvolvimento: {
+        server: "BASETESTE.database.windows.net",
+        user: "usuariotestes",
+        password: "senhatestes",
+        database: "BASETESTE",
+        options: {
+            encrypt: true
+        }
+    }
+}
+
+var sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+var perfil = desenvolvimento ? 'desenvolvimento' : 'producao';
+
+function conectar() {
+    //   return sql.connect(configuracoes[perfil])
+    return new sql.ConnectionPool(configuracoes[perfil]).connect();
+}
+
+function conectar2() {
+    return sql.connect(configuracoes[perfil])
+        //   return new sql.ConnectionPool(configuracoes[perfil]).connect();  
+}
+
+module.exports = {
+    conectar2: conectar2,
+    conectar: conectar,
+    sql: sql,
+    connection: connection
+}
